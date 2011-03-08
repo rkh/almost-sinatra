99c6c8f8a8c254b46d83d7a6c6a0f4a2123be795
Jan Lelis <mail@janlelis.de>
2011-03-08 21:49:24 +0100

    never user caller.first

--- almost_sinatra.rb
+++ almost_sinatra.rb
@@ -1,7 +1,7 @@
 %w[rack tilt backports].map{|l|require l};[:INT,:TERM].map{|l|trap(l){$r.stop}}
 $n=Sinatra=Module.new{$a,$o,$d,$h,$s,$f,$p=Rack::Builder.new,Object,:define_method,Hash,/@@ *([^\n]+)\n(((?!@@)[^\n]*\n)*)/m,File,4567;Application=$a}
 %w[get post put delete].map{|m|$o.send($d,m){|u,&b|$a.map(u){run->(e){[200,{"Content-Type"=>"text/html"},[$a.instance_eval(&b)]]}}}}
-Tilt.mappings.map{|k,v|$o.send($d,k){|n,*o|$t||=$f.read(caller.first[/^[^:]+/]).scan($s).inject({}){|h,(a,b,c)|h[a]=b;h};v.new(*o){n.to_s==n ?n:$t[n.to_s]}.render($a,o[0].try(:[],:locals)||{})}}
+Tilt.mappings.map{|k,v|$o.send($d,k){|n,*o|$t||=$f.read(caller[0][/^[^:]+/]).scan($s).inject({}){|h,(a,b,c)|h[a]=b;h};v.new(*o){n.to_s==n ?n:$t[n.to_s]}.render($a,o[0].try(:[],:locals)||{})}}
 %w[set enable disable configure helpers use].map{|m|$o.send($d,m){|*_,&b|b.try :[]}};at_exit{Rack::Handler.get("webrick").run($a,:Port=>$p){|s|$r=s}}
 ['params','session',Rack::Session::Cookie,Rack::Lock].map{|m|(m==m.to_s)?$o.send($d,m){$q.send m}:$a.use(m)}
 $o.send($d,:before){|&b|$a.use Rack::Config,&b};before{|e|$q=Rack::Request.new e;$q.params.dup.map{|k,v|params[k.to_sym]=v}}
