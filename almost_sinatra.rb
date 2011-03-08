aae45136b8e3463063e4501b2667166e15bed12a
Jan Lelis <mail@janlelis.de>
2011-03-08 22:14:58 +0100

    remove dup without knowing exact the consequences °_°

--- almost_sinatra.rb
+++ almost_sinatra.rb
@@ -4,5 +4,5 @@ $n=Sinatra=Module.new{$a,$o,$d,$s,$p=Rack::Builder.new,Object,:define_method,/@@
 Tilt.mappings.map{|k,v|$o.send($d,k){|n,*o|$t||=File.read(caller[0][/^[^:]+/]).scan($s).inject({}){|h,(a,b,c)|h[a]=b;h};v.new(*o){(z=n.to_s)==n ?n:$t[z]}.render $a,o[0].try(:[],:locals)||{}}}
 %w[set enable disable configure helpers use].map{|m|$o.send($d,m){|*_,&b|b.try :[]}};END{Rack::Handler.get("webrick").run($a,:Port=>$p){|s|$r=s}}
 %w[params session].map{|m|$o.send($d,m){$q.send m}};[Rack::Session::Cookie,Rack::Lock].map{|m|$a.use m}
-$o.send($d,:before){|&b|$a.use Rack::Config,&b};before{|e|$q=Rack::Request.new e;$q.params.dup.map{|k,v|params[k.to_sym]=v}}
+$o.send($d,:before){|&b|$a.use Rack::Config,&b};before{|e|$q=Rack::Request.new e;$q.params.map{|k,v|params[k.to_sym]=v}}
 puts"== Almost #$n/No Version has taken the stage on #$p for development with backup from Webrick"
