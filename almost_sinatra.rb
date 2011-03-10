4444a2c93f92005e7777f012969f3d5692d44828
Jan Lelis <mail@janlelis.de>
2011-03-11 00:13:31 +0100

    lines for bytes allow :D?

--- almost_sinatra.rb
+++ almost_sinatra.rb
@@ -1,5 +1,9 @@
 %w.rack tilt backports INT TERM..map{|l|trap(l){R.stop}rescue require l}
-$n=Sinatra=Module.new{extend Rack;a,D,S,p,q,Application=Builder.new,Object.method(:define_method),/@@ *([^\n]+)\n(((?!@@)[^\n]*\n)*)/m,4567,a
+$n=Sinatra=Module.new{extend Rack;a,D,S,p,q,Application=Builder.new,Object.method(:define_method),/@@ *([^
+]+)
+(((?!@@)[^
+]*
+)*)/m,4567,a
 %w[get post put delete].map{|m|D.(m){|u,&b|a.map(u){run->(e){[200,{"Content-Type"=>"text/html"},[a.instance_eval(&b)]]}}}}
 Tilt.mappings.map{|k,v|D.(k){|n,*o|$t||=(h={};File.read(caller[0][/^[^:]+/]).scan(S){|a,b|h[a]=b};h);v.new(*o){n.to_s==n ?n:$t[n.to_s]}.render(a,o[0].try(:[],:locals)||{})}}
 %w[set enable disable configure helpers use register].map{|m|D.(m){|*_,&b|b.try :[]}};END{Handler.get("webrick").run(a,Port:p){|s|R=s}}
