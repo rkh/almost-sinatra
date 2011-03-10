2ecd1b413765195dd4c6f3c5f64bfed81fac2cc6
Jan Lelis <mail@janlelis.de>
2011-03-11 00:31:03 +0100

    yes, ruby rocks

--- almost_sinatra.rb
+++ almost_sinatra.rb
@@ -6,7 +6,7 @@ $n=Sinatra=Module.new{extend Rack;a,D,S,p,q,Application=Builder.new,Object.metho
 )*)/m,4567,a
 %w[get post put delete].map{|m|D.(m){|u,&b|a.map(u){run->(e){[200,{"Content-Type"=>"text/html"},[a.instance_eval(&b)]]}}}}
 Tilt.mappings.map{|k,v|D.(k){|n,*o|$t||=(h={};File.read(caller[0][/^[^:]+/]).scan(S){|a,b|h[a]=b};h);v.new(*o){n==(z=n.to_s)?n:$t[z]}.render(a,o[0].try(:[],:locals)||{})}}
-%w[set enable disable configure helpers use register].map{|m|D.(m){|*_,&b|b.try :[]}};END{Handler.get("webrick").run(a,Port:p){|s|R=s}}
+%w[set enable disable configure helpers use register].map{|m|D.(m){|*,&b|b.try :[]}};END{Handler.get("webrick").run(a,Port:p){|s|R=s}}
 %w[params session].map{|m|D.(m){q.send m}};a.use Session::Cookie;a.use Lock
 D.(:before){|&b|a.use Rack::Config,&b};before{|e|q=Request.new e;q.params.dup.map{|k,v|params[k.to_sym]=v}}
 }
