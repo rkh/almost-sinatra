4d747771cfaf98560bed4a79d806b505246252e0
Jan Lelis <mail@janlelis.de>
2011-03-11 01:22:46 +0100

    WEBrick is BIG :D

--- almost_sinatra.rb
+++ almost_sinatra.rb
@@ -6,8 +6,8 @@ Tilt.mappings.map{|k,v|D.(k){|n,*o|$t||=(h={};File.read(caller[0][/^[^:]+/]).sca
 (((?!@@)[^
 ]*
 )*)/m){h[$1]=$2};h);v.new(*o){n==(z=n.to_s)?n:$t[z]}.render(a,o[0].try(:[],:locals)||{})}}
-%w[set enable disable configure helpers use register].map{|m|D.(m){|*,&b|b.try :[]}};END{Handler.get("webrick").run(a,Port:P){|s|R=s}}
+%w[set enable disable configure helpers use register].map{|m|D.(m){|*,&b|b.try :[]}};END{Handler.get(W).run(a,Port:P){|s|R=s}}
 %w[params session].map{|m|D.(m){q.send m}};a.use Session::Cookie;a.use Lock
 D.(:before){|&b|a.use Rack::Config,&b};before{|e|q=Request.new e;q.params.dup.map{|k,v|params[k.to_sym]=v}}
 }
-puts"== almost #$n/No Version has taken the stage on #{P} for development with backup from Webrick"
+puts"== almost #$n/No Version has taken the stage on #{P} for development with backup from "+W="WEBrick"
